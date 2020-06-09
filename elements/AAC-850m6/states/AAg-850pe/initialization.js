1b8b6cbeb6bf91536f1c7f70a6313e636ba21bde
Alessandro Jeanteur <aless.jeant@gmail.com>
2020-06-09 12:22:35 +0200

    Automatic commit from Bubble

--- elements/AAC-850m6/states/AAg-850pe/initialization.js
+++ elements/AAC-850m6/states/AAg-850pe/initialization.js
@@ -1,9 +1,3 @@
 function(properties, context) {
-
-	if(properties.empty==true && properties.initial_content==null){
-        return false;
-    } else {
-        return true; 
-    }
-
+	return properties.empty === true && !properties.initial_content;
 }
